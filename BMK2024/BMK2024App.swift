70a753445f4e840d66d9f53b608c854cd00e5a3f
m1 <mbp98k@gmail.com>
2024-11-09 04:03:15 +0800

    init project

---
+++ BMK2024/BMK2024App.swift
@@ -0,0 +1,17 @@
+//
+//  BMK2024App.swift
+//  BMK2024
+//
+//  Created by Scott on 2024/11/9.
+//
+
+import SwiftUI
+
+@main
+struct BMK2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BMK2024/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BMK2024
+//
+//  Created by Scott on 2024/11/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
